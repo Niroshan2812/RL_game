596b0b2cabfe36cb15283b2c56395cc44e246ba9
Niroshan <niroshan9812@gmail.com>
2025-08-18 23:05:22 +0530

    display envirement

--- snake_game.py
+++ snake_game.py
@@ -5,5 +5,18 @@ pygame.init()
 font = pygame.font.Font('arial.ttf', 25)
 
 class SnakeGame:
-    def __init__(self):
-        print ("initial")
+    def __init__(self, w=640,h=480):
+        self.w = w
+        self.h=h
+
+        #game display envirement 
+        self.display =  pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption("Snake")
+        self.clock = pygame.time.Clock()
+
+        #print ("initial")
+
+
+
+if __name__ =='__main__':
+    game = SnakeGame()
