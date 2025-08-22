32a481e58cfe4166058b7f5f289178306a2feeef
Niroshan <niroshan9812@gmail.com>
2025-08-22 11:11:04 +0530

    final implementation

--- agent.py
+++ agent.py
@@ -4,6 +4,7 @@ import numpy as np
 from collections import deque
 from snake_game import SnakeGame, Direction, Point
 from model import Linner_QNet, Qtrainner
+from helper import plot
 
 MAX_MEMORY = 100000
 BATCH_SIZE = 1000
@@ -138,6 +139,12 @@ def train():
                 agent.model.save()
             
             print ('Game: ', agent.n_games, 'Score: ', score, 'Record: ', record)
+
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
 
 if __name__ == '__main__':
     train()
